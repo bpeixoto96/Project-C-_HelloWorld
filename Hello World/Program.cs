2f7ded4281d0f1fe583f55d9a795608e231077cd
bpeixoto96 <peixotobruno17@gmail.com>
2022-07-12 22:57:51 -0300

    ProjectC#_Hello World

---
+++ Hello World/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello_World_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World,Eu sou o Bruno!");
+        }
+    }
+}
